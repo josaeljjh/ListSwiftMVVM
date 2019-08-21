18da2058ee3fddfc9becd49f432c5e5d73c1b899
josael.hernandez <Elannin1>
2019-08-21 12:01:12 -0600

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Josael Hernandez on 8/21/19.
+//  Copyright © 2019 Josael Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
